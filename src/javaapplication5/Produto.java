a99177f0e541ba1824d65eab4f9c72317c467453
Ruan Oliveira <ruanamaral40@gmail.com>
2023-02-28 21:07:17 -0300

    enviando arquivos para o repositorio

---
+++ src/javaapplication5/Produto.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author ruana
+ */
+public class Produto {
+    private String nome;
+
+    public Produto(String nome, float peso, float preco) {
+        this.nome = nome;
+        this.peso = peso;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    private float peso;
+    private float preco;
+}
